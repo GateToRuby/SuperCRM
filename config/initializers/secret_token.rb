1c9612951e2cf2f32968804bc8653b004e574169
Jacek Podkanski <ruby.object@googlemail.com>
2013-05-02 15:08:18 +0100

    created new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SuperCRM::Application.config.secret_token = 'be7dd9f32a7c0da750f79c5eedc3dd95e048f2b360eb7294d10c192e85d06e2f0ea9311da89f1cbdd9a2e0afb2a920c01d382e9482981be10832a35aa13f467e'
