895713b44c428d3655bbba764cbff9b06f34e44f
andreadirocco <andreadrcc@gmail.com>
2013-05-03 17:42:16 +0200

    ran migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130502152335) do
+ActiveRecord::Schema.define(:version => 20130503152302) do
 
   create_table "user_sessions", :force => true do |t|
     t.datetime "created_at", :null => false
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(:version => 20130502152335) do
   create_table "users", :force => true do |t|
     t.datetime "created_at",                        :null => false
     t.datetime "updated_at",                        :null => false
-    t.string   "login",                             :null => false
+    t.string   "username",                          :null => false
     t.string   "crypted_password",                  :null => false
     t.string   "password_salt",                     :null => false
     t.string   "persistence_token",                 :null => false
